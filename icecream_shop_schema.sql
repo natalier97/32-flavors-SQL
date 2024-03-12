330b441bf44b81bbd0c9b14a289dca8ebcdad807
Natalie <rivero_natalie@outlook.com>
2024-03-11 21:12:25 -0400

    updates sales, and employees timesheets

--- icecream_shop_schema.sql
+++ icecream_shop_schema.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS icecream_inventory;
 DROP TABLE IF EXISTS cones_inventory;
 DROP TABLE IF EXISTS sales;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS timesheets;
 
 CREATE TABLE icecream_inventory(
     id SERIAL PRIMARY KEY,
@@ -21,7 +22,8 @@ CREATE TABLE cones_inventory(
 
 CREATE TABLE sales(
     id SERIAL PRIMARY KEY,
-    item VARCHAR(50),
+    cone_id INTEGER,
+    icecream_id INTEGER,
     price decimal(4,2),
     quantity INTEGER
 );
@@ -30,9 +32,15 @@ CREATE TABLE employees(
     id SERIAL PRIMARY KEY,
     employee_name VARCHAR(75),
     position VARCHAR(50),
-    pay_rate_per_hour decimal(8,2),
+    pay_rate_per_hour decimal(8,2)
+);
+
+CREATE TABLE timesheets(
+    id SERIAL PRIMARY KEY,
+    employee_id INTEGER,
     time_clocked_in TIMESTAMP WITH TIME ZONE,
     time_clocked_out TIMESTAMP WITH TIME ZONE
+
 );
 
 
@@ -43,3 +51,5 @@ CREATE TABLE employees(
 \COPY sales FROM '/home/natalie/code_platoon/assignments/7th-week-assignments/32-flavors-1-SQL/data/sales.csv' WITH CSV HEADER;
 
 \COPY employees FROM '/home/natalie/code_platoon/assignments/7th-week-assignments/32-flavors-1-SQL/data/employees.csv' WITH CSV HEADER;
+
+\COPY timesheets FROM '/home/natalie/code_platoon/assignments/7th-week-assignments/32-flavors-1-SQL/data/timesheets.csv' WITH CSV HEADER;
